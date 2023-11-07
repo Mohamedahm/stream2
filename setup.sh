25db8dbe2e5d6205b7eb7be78d2b280471ee69f9
Mohamedahm <Mohamed_31383@hotmail.com>
2023-11-07 17:58:30 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 
 sudo apt-get update
 sudo apt-get install -y libgl1-mesa-glx
+
 sudo apt-get install portaudio19-dev
+sudo yum install portaudio-devel
+sudo dnf install portaudio-devel
+
 sudo apt-get install libasound-dev
 sudo apt-get install libportaudio2
 pip install -r requirements.txt
