cfdd0e9e811ff2b04eeb40a84178f5cb9ce378bf
Mohamedahm <Mohamed_31383@hotmail.com>
2023-11-09 12:57:38 +0400

    Update stream.py

--- stream.py
+++ stream.py
@@ -32,18 +32,8 @@ drive_name = 'myspacedrive'
 drive = project.Drive(drive_name)
 uploaded_file = st.file_uploader("Choose a file")
 
-# If user attempts to upload a file.
-if uploaded_file is not None:
-    bytes_data = uploaded_file.getvalue()
-
-    # Show the image filename and image.
-    st.write(f'filename: {uploaded_file.name}')
-    st.audio(bytes_data)
-
-    # Upload the image to deta using put with filename and data.
-    drive.put(uploaded_file.name, data=bytes_data)
-
 count = 0
+
  # object classes
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
                "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
@@ -277,6 +267,16 @@ def second_page():
         st.write(text)
         # db.put(audio_file_name)
         # Optionally, clean up the audio file after processing
+
+        # If user attempts to upload a file.
+        bytes_data = uploaded_file.getvalue()
+    
+        # Show the image filename and image.
+        st.write(f'filename: {uploaded_file.name}')
+        st.audio(bytes_data)
+    
+        # Upload the image to deta using put with filename and data.
+        drive.put(uploaded_file.name, data=bytes_data)
         os.remove(audio_file_name)
     
     
